f11438da98b433395bad4af29bc16e160bded033
HasanFerit <hasanferit@hasanferit-VB.(none)>
2017-10-27 16:37:28 +0300

    Add thumbnail picture at top of article content

--- trumpcrawler/crawl/views.py
+++ trumpcrawler/crawl/views.py
@@ -33,36 +33,39 @@ def get_home(request):
     headlines = []
     descriptions = []
     uris = []
+    thumbnails = []
     head_indices = []
     for news in trump_news: 
-        news_json = json.loads(news.replace("\\'", "'").replace('\\\\"',"'"),  strict=False)
-        headlines.append(news_json['headline'])
+        article_json = json.loads(news.replace("\\'", "'").replace('\\\\"',"'"),  strict=False)
+        headlines.append(article_json['headline'])
         head_indices.append(trump_news.index(news))
-        descriptions.append(news_json['description'])
-        uris.append(news_json['uri'])
+        descriptions.append(article_json['description'])
+        thumbnails.append(article_json['thumbnail'])
+        uris.append(article_json['uri'])
 
     headlines_zipped = zip(head_indices, headlines)
     return render(request, 'crawl/main.html', {'headlines_zipped': headlines_zipped, 'descriptions':descriptions, 
-        'uris': uris})
+        'uris': uris, 'thumbnails': thumbnails})
 
 @csrf_exempt
 def get_content(request):
 
-    index = request.POST.get('index')
-    index = int(index)
-    print(index)
+    index = int(request.POST.get('index'))
+
     uris = request.POST.get('uris')
     uris = uris.split('&#39;, &#39;')
     uris[0] = uris[0].replace('[&#39;','')
     uris[-1] = uris[-1].replace('&#39;]','')
     uri = uris[index]
-    # opener = urllib2.build_opener()
-    # opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-    # page_opener = opener.open('http://edition.cnn.com'+uri)
-    print('http://edition.cnn.com/'+uri)
+
+    thumbnails = request.POST.get('thumbnails')
+    thumbnails = thumbnails.split('&#39;, &#39;')
+    thumbnails[0] = thumbnails[0].replace('[&#39;','')
+    thumbnails[-1] = thumbnails[-1].replace('&#39;]','')
+    thumbnail = thumbnails[index]
+
     html = urlopen('http://edition.cnn.com'+uri)
     fw = open('source_file_2', 'w')
-    # fw.write(str(page_opener.read()))
     fw.write(str(html.read()))
     fw.close()
     fr = open('source_file_2', 'r')
@@ -74,4 +77,4 @@ def get_content(request):
             content += match
     content = content.replace('<div class=\"zn-body__paragraph\">', '')
     content = content.replace('</div>', '')
-    return JsonResponse({'content': content})
+    return JsonResponse({'content': content, 'thumbnail': thumbnail})
