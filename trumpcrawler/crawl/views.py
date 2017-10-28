4fb30fc27b619534b479c69044ffc4c83f78fbf8
HasanFerit <hasanferit@hasanferit-VB.(none)>
2017-10-28 13:42:32 +0300

    Catch changes in py2 branch

--- trumpcrawler/crawl/views.py
+++ trumpcrawler/crawl/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import  JsonResponse
-# import urllib2
 from urllib.request import urlopen
 import re
 import json
@@ -9,13 +8,40 @@ import json
 # Create your views here.
 
 def get_home(request):
-    
-    # opener = urllib2.build_opener()
-    # opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-    # page_opener = opener.open('http://edition.cnn.com/')
+    return render(request, 'crawl/main.html', {})
+
+def get_tweets(request):
+    html = urlopen('https://twitter.com/realDonaldTrump')
+    fw = open('source_file', 'w')
+    fw.write(str(html.read()))
+    fw.close()
+
+    fr = open('source_file', 'r')
+    lines = fr.readlines()
+    fr.close()
+    trump_tweets = []
+    for line in lines:
+        matches = re.findall(r'<p class="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text" lang="en" data-aria-label-part="0">.*?</p>', line, re.M|re.I)
+        for match in matches:
+            match = re.sub('<p class="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text" lang="en" data-aria-label-part="0">', '', match)
+            match = re.sub('</p>', '', match)
+            mention_hrefs = re.findall(r'<a href=".*?" class', match)
+            for mention_href in mention_hrefs:
+                
+                mention = re.findall(r'".*?"', mention_href)[0]
+                if 'http' in mention:
+                    continue
+                match = re.sub(mention, '\"https://twitter.com'+mention[1:], match)
+            
+            trump_tweets.append(match)
+
+
+    return render(request, 'crawl/tweets.html', {'tweets': trump_tweets})
+
+
+def get_articles(request):
     html = urlopen('http://edition.cnn.com/')
     fw = open('source_file', 'w')
-    # fw.write(str(page_opener.read()))
     fw.write(str(html.read()))
     fw.close()
 
@@ -40,41 +66,54 @@ def get_home(request):
         headlines.append(article_json['headline'])
         head_indices.append(trump_news.index(news))
         descriptions.append(article_json['description'])
-        thumbnails.append(article_json['thumbnail'])
-        uris.append(article_json['uri'])
+        thumbnails.append(str(article_json['thumbnail']))
+        uris.append(str(article_json['uri']))
+
+    titles = []
+    authors = []
+    datetimes = []
+    contents = []
+    for uri in uris:
+        page_opener = opener.open('http://edition.cnn.com'+uri)
+        fw = open('source_file_2', 'w')
+        fw.write(str(page_opener.read()))
+        fw.close()
+        fr = open('source_file_2', 'r')
+        lines = fr.readlines()
+        content = ''
+        title = ''
+        author = ''
+        datetime = ''
+        for line in lines:
+            matches = re.findall(r'class=\"zn-body__paragraph.*?\">.*?<\/div>', line, re.M|re.I)
+            for match in matches:
+                content += match
+            matches = re.findall(r'<h1 class="pg-headline">.*?<\/h1>', line)
+            for match in matches:
+                title += match
+            matches = re.findall(r'<span class="metadata__byline__author">.*?<\/span>', line)
+            for match in matches:
+                author += match
+            matches = re.findall(r'<p class="update-time">.*?<\/span', line)
+            for match in matches:
+                datetime += match
+        content = re.sub(r'class=\"zn-body__paragraph.*?\">', '', content) #content.replace()
+        content = re.sub('</div>', '<br>', content)
+
+        title = re.sub(r'<h1 class="pg-headline">', '', title) #content.replace()
+        title = re.sub('<\/h1>', '', title)
+
+        author = re.sub(r'<span class="metadata__byline__author">', '', author) #content.replace()
+        author = re.sub('<\/span>', '', author)
+
+        datetime = re.sub(r'<p class="update-time">', '', datetime) #content.replace()
+        datetime = re.sub('<\/span', '', datetime)
+
+        contents.append(content)
+        titles.append(title)
+        authors.append(author)
+        datetimes.append(datetime)
 
     headlines_zipped = zip(head_indices, headlines)
-    return render(request, 'crawl/main.html', {'headlines_zipped': headlines_zipped, 'descriptions':descriptions, 
-        'uris': uris, 'thumbnails': thumbnails})
-
-@csrf_exempt
-def get_content(request):
-
-    index = int(request.POST.get('index'))
-
-    uris = request.POST.get('uris')
-    uris = uris.split('&#39;, &#39;')
-    uris[0] = uris[0].replace('[&#39;','')
-    uris[-1] = uris[-1].replace('&#39;]','')
-    uri = uris[index]
-
-    thumbnails = request.POST.get('thumbnails')
-    thumbnails = thumbnails.split('&#39;, &#39;')
-    thumbnails[0] = thumbnails[0].replace('[&#39;','')
-    thumbnails[-1] = thumbnails[-1].replace('&#39;]','')
-    thumbnail = thumbnails[index]
-
-    html = urlopen('http://edition.cnn.com'+uri)
-    fw = open('source_file_2', 'w')
-    fw.write(str(html.read()))
-    fw.close()
-    fr = open('source_file_2', 'r')
-    lines = fr.readlines()
-    content = ''
-    for line in lines:
-        matches = re.findall(r'<div class=\"zn-body__paragraph\">.*?</div>', line, re.M|re.I)
-        for match in matches:
-            content += match
-    content = content.replace('<div class=\"zn-body__paragraph\">', '')
-    content = content.replace('</div>', '')
-    return JsonResponse({'content': content, 'thumbnail': thumbnail})
+    return render(request, 'crawl/cnn.html', {'headlines_zipped': headlines_zipped, 'descriptions':descriptions, 
+        'uris': uris, 'thumbnails': thumbnails, 'contents': contents, 'titles':titles, 'authors':authors, 'datetimes': datetimes})
